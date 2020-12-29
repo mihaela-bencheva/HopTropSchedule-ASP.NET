4c726bcf81bbc23c0347e702bb0bbf75ad49c4bd
mihaela-bencheva <63971085+mihaela-bencheva@users.noreply.github.com>
2020-12-29 22:10:54 +0200

    Authentication

--- ScheduleAPI/Startup.cs
+++ ScheduleAPI/Startup.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Data.DataConnection;
@@ -14,6 +16,8 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.IdentityModel.Tokens;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Services;
 using Services.ApiServices;
 
@@ -36,6 +40,32 @@ namespace ScheduleAPI
             services.AddScoped<ScheduleDbContext>();
             services.AddScoped<IdentityService>();
             services.AddScoped<ScheduleService>();
+
+            var path = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            var file = Path.Combine(path, "tokenconfig.json");
+            StreamReader sr = new StreamReader(file);
+            string content = sr.ReadToEnd();
+            var jsonObject = (JObject)JsonConvert.DeserializeObject(content);
+            string salt = jsonObject["secret"].Value<string>();
+
+
+            services.AddAuthentication(opt => {
+                opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+            })
+            .AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = "http://localhost:5000",
+                    ValidAudience = "http://localhost:5000",
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(salt))
+                };
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
